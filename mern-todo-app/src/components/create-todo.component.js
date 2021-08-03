fe683f312816a33cb03324236b99bafee4d1f94c
zuwannn <suwanneethongtha@gmail.com>
2021-08-04 00:54:23 +0700

    update

--- mern-todo-app/src/components/create-todo.component.js
+++ mern-todo-app/src/components/create-todo.component.js
@@ -41,7 +41,7 @@ export default class CreateTodo extends Component {
         console.log(`Todo Description: ${this.state.todo_description}`);
         console.log(`Todo Responsible: ${this.state.todo_responsible}`);
         console.log(`Todo Priority: ${this.state.todo_priority}`);
-        console.log(`Todo Completed: ${this.state.todo_completed}`);
+        //console.log(`Todo Completed: ${this.state.todo_completed}`);
         
         this.setState({
             todo_description: '',
@@ -53,8 +53,66 @@ export default class CreateTodo extends Component {
     
     render() {
         return (
-            <div>
-                <p>Welcome to Create Todo Component!!</p>
+            <div style={{marginTop: 20}}>
+                <h3>Create New Todo</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="Form-group">
+                        <label>Description: </label>
+                        <input  type="text"
+                                className="form-control"
+                                value={this.state.todo_description}
+                                onChange={this.onChangeTodoDescription}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label>Responsible: </label>
+                        <input type="text"
+                                className="form-control"
+                                value={this.state.todo_responsible}
+                                onChange={this.onChangeTodoResponsible} 
+                        />
+                    </div>
+                    <div className="form-group">
+                        <div className="form-check form-check-inline">
+                            <input type="radio"
+                                    className="form-check-input" 
+                                    name="priorityOptions"
+                                    id="priorityLow"
+                                    value="Low"
+                                    checked={this.state.todo_priority==='Low'}
+                                    onChange={this.onChangeTodoPriority}
+                            />
+                            <label className="from-check-label">Low</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input type="radio"
+                                    className="form-check-input" 
+                                    name="priorityOptions"
+                                    id="priorityMedium"
+                                    value="Medium"
+                                    checked={this.state.todo_priority==='Medium'}
+                                    onChange={this.onChangeTodoPriority}
+                            />
+                            <label className="from-check-label">Medium</label>
+                        </div>
+                        <div className="form-check form-check-inline">
+                            <input type="radio"
+                                    className="form-check-input" 
+                                    name="priorityOptions"
+                                    id="priorityHigh"
+                                    value="High"
+                                    checked={this.state.todo_priority==='High'}
+                                    onChange={this.onChangeTodoPriority}
+                            />
+                            <label className="from-check-label">High</label>
+                        </div>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Create Todo" className="btn btn-primary" />
+                    </div>
+
+                </form>
             </div>
         )
     }
